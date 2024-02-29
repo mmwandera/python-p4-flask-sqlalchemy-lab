a1228501653079ee2fb4aefa2c07cd9dcbe77a70
mmwandera <mmwandera74@gmail.com>
2024-02-29 14:27:16 +0300

    completed assignment

--- server/models.py
+++ server/models.py
@@ -7,17 +7,38 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
+# The Zookeeper model should contain a name, a birthday, and a list of animals that they take care of.
 class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    birthday = db.Column(db.String)
 
+    animals = db.relationship('Animal', back_populates='zookeeper')
+
+# The Enclosure model should contain an environment (grass, sand, or water), an open_to_visitors boolean, and a list of animals.
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
 
     id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String)
+    open_to_visitors = db.Column(db.Boolean)
 
+    animals = db.relationship('Animal', back_populates='enclosure')
+
+# The Animal model should contain a name, a species, a zookeeper, and an enclosure.
 class Animal(db.Model):
     __tablename__ = 'animals'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    species = db.Column(db.String)
+
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeepers.id'))
+    enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosures.id'))
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeepers.id'))
+    enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosures.id'))
+
+    zookeeper = db.relationship('Zookeeper', back_populates='animals')
+    enclosure = db.relationship('Enclosure', back_populates='animals')
